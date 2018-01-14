2e862fb0cacba0620d4966926cd4a24bb13b4698
thordurb12 <thordur@gangverk.is>
2018-01-14 19:01:49 +0000

    Changes

--- dev/render-server/views/Ripple.js
+++ dev/render-server/views/Ripple.js
@@ -1,16 +1,14 @@
 module.exports = (function Ripple(color) {
-    var _scale = 0;
+    var _scale = 1;
     var _killThisRipple = false;
     var _maxScale = 80;
     var _color = color;
     var _scalingSpeed = 0.02;
 
-    var _xInit = 0;
-    var _yInit = 0;
-
+    var _xTranslate = 0;
+    var _yTranslate = 0;
     var _width = 8;
     var _height = 6;
-
 
     function scale(point, offset) {
         return point;
@@ -21,24 +19,25 @@ module.exports = (function Ripple(color) {
 
         ctx.save();
 
-        var xTranslate = (cw - _width * _scale) / 2;
-        var ytTranslate = (ch - _height * _scale) / 2;
+        _xTranslate = (cw - _width * _scale) / 2;
+        _yTranslate = (ch - _height * _scale) / 2;
 
-        ctx.translate(xTranslate, ytTranslate);
+        ctx.translate(_xTranslate, _yTranslate);
 
         ctx.strokeStyle = _color;
         ctx.lineWidth = 1;
 
         ctx.beginPath();
         
-		ctx.moveTo(_scale*(_xInit+1), _scale*_yInit);
-		ctx.lineTo(_scale*_xInit, _scale*(_yInit+_height));
+        ctx.moveTo(_scale*1, 0);
+        
+		ctx.lineTo(0, _scale*_height);
 		
-		ctx.lineTo(_scale*(_xInit), _scale*(_yInit+_height-1));
+		ctx.lineTo(0, _scale*(_height-1));
 
-		ctx.lineTo(_scale*(_xInit+_width), _scale*(_yInit+_height));
+		ctx.lineTo(_scale*_width, _scale*_height);
 
-		ctx.lineTo(_scale*(_xInit+_width-3), _scale*(_yInit));
+		ctx.lineTo(_scale*(_width-3), 0);
 
         ctx.closePath();
         
