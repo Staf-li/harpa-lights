0572a38fd5875b848708f543c82e863ffa503c3a
thordurb12 <thordur@gangverk.is>
2018-02-01 23:29:13 +0000

    Colors I like

--- dev/render-server/views/visual-renderer.js
+++ dev/render-server/views/visual-renderer.js
@@ -8,7 +8,15 @@ module.exports = function VisualRenderer() {
   var _heartData = [0, 0, 0];
 
   var _hearts = [
-    new BaseHeart('rgb(255, 0 , 0)', 0.015),
+    // new BaseHeart('rgb(64, 102, 160)', 0.020),
+    // new BaseHeart('rgb(255, 253, 125)', 0.010),
+    // new BaseHeart('rgb(255, 0, 0)', 0.015),
+
+    // new BaseHeart('rgb(0, 0, 255)', 0.020),
+    // new BaseHeart('rgb(255, 0, 0)', 0.015),
+    // new BaseHeart('rgb(0, 255, 0)', 0.015),
+
+    new BaseHeart('rgb(255, 0, 0)', 0.015),
     new BaseHeart('rgb(248, 231, 28)', 0.010),
     new BaseHeart('rgb(111, 162, 248)', 0.020),
   ];
